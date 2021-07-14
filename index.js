89447adc68ab09ad15d0ddc213ac3ee7fa2a1868
Cam <cam.ball96@hotmail.com>
2021-07-14 18:27:14 +1000

    added a heap of js to get node to run and ask questions i just now need it to save

--- index.js
+++ index.js
@@ -1,13 +1,109 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const genMarkD = require("./utils/generateMarkdown.js");
+const fs = require('fs');
+valid = (input)=> {
+    if(input) {
+        return true;
+    }
+    return "Please fill in";
+}
+
+
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questionsArr = [
+    {
+        message: 'Please enter your projects title',
+        name: "title",
+        type: "input",
+        validate: valid,
+    },
+    {
+        message: "Why did you build this project?",
+        name: "question1",
+        type: "input",
+        validate: valid,
+    },
+    {
+        message: "What was your motivation?",
+        name: "question2",
+        type: "input",
+        validate: valid,
+    },
+    {
+        message: "What problem/s does it solve?",
+        name: "question3",
+        type: "input",
+        validate: valid,
+    },
+    {
+        message: "What did you learn?",
+        name: "question4",
+        type: "input",
+        validate: valid,
+    },
+    {
+        message: "What were the main challenges you faced?",
+        name: "question5",
+        type: "input",
+        validate: valid,
+    },
+    {
+        message: "What future features could you incorperate in future development?",
+        name: "question6",
+        type: "input",
+        validate: valid,
+    },
+    {
+        message: "How to install this application",
+        name: "Installation",
+        type: "input",
+        validate: valid,
+    },
+    {
+        message: "List the features or technologies used in this application",
+        name: "Technologies",
+        type: "input",
+        validate: valid,
+    },
+    /*{
+        message: "What licence does the project have?",
+        name: "Licence",
+        Choices: ["MIT", "Apache 2.0", "ISC", "None"],
+        type: "list",
+        validate: valid,
+    },*/
+    {
+        message: "Supply your github username and email for contact purposes",
+        name: "contact",
+        type: "input",
+        validate: valid,
+    },
+    {
+        message: "How can people contribute?",
+        name: "contributions",
+        type: "input",
+        validate: valid,
+    },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
+function askedQuestion(questions){
+    inquirer.prompt(questions)
+    .then((answers) => {
+    writeToFile("newREADME.md", answers);
+    });
+}
+
+//TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, )
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    askedQuestion(questionsArr);
+};
 
-// Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,11 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(licence === "None") return "";
+  else if (licence === "MIT") return "https://img.shields.io/badge/License-MIT-green.svg";
+  else if (licence === "ISC") return "https://img.shields.io/badge/License-ISC-green.svg";
+  else if (licence === "Apache 2.0") return "https://img.shields.io/badge/License-Apache%202.0-green.svg";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -8,12 +13,52 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return `## Licence\n![${licence}](${renderLicenseLink(licence)})`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
 
+  return `# ${data.title}
+  ${renderLicenseSection(data.licence)}
+
+## Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Technologies](#technologies)
+- [Licence](#licence)
+- [Contact](#contact)
+- [Contributions](#contributions)
+
+
+  ## Description
+  \nA little on why I built this project was because: ${data.question1}
+
+  \nWhat motivated me to build this project was: ${data.question2}
+
+  \nThe problems I managed to solve by building this application included: ${data.question3}
+
+  \nHere's a little on what I learnt whilst building this application: ${data.question4}
+
+  \nSome of the main challenges I face included: ${data.question5}
+
+  \nI would like to incorperate some of the following features in future development: ${data.question6}
+
+## Installation
+\nTo install this application simply: ${data.installation}
+
+## Technologies
+\nSome of the technologies or features this application displays includes: ${data.technologies}
+
+## Contact
+\nYou can reach out or see more on my projects by contacting me on the following: ${data.contact}
+
+## Contributions
+\n${data.contributions}
+
+## Acknowledgements
+\n
 `;
 }
 
